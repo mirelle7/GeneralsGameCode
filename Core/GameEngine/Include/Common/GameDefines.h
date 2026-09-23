4cb7b83c942dfd19d867dbab6757c69bca38b918
mirelle7 <115191165+mirelle7@users.noreply.github.com>
2026-09-24 01:08:34 +0200

    fix(pathfinder): Use a 20 unit pathfind cell and fixed pathfinding for large maps
    Double PATHFIND_CELL_SIZE so the pathfind grid of very large maps stays manageable, and
    disable RETAIL_COMPATIBLE_PATHFINDING and RETAIL_COMPATIBLE_PATHFINDING_ALLOCATION.

--- Core/GameEngine/Include/Common/GameDefines.h
+++ Core/GameEngine/Include/Common/GameDefines.h
@@ -109,12 +109,12 @@
 
 // This is here to easily toggle between the retail compatible with fixed pathfinding fallback and pure fixed pathfinding mode
 #ifndef RETAIL_COMPATIBLE_PATHFINDING
-#define RETAIL_COMPATIBLE_PATHFINDING (1)
+#define RETAIL_COMPATIBLE_PATHFINDING (0)
 #endif
 
 // This is here to easily toggle between the retail compatible pathfinding memory allocation and the new static allocated data mode
 #ifndef RETAIL_COMPATIBLE_PATHFINDING_ALLOCATION
-#define RETAIL_COMPATIBLE_PATHFINDING_ALLOCATION (1)
+#define RETAIL_COMPATIBLE_PATHFINDING_ALLOCATION (0)
 #endif
 
 #ifndef RETAIL_COMPATIBLE_CIRCLE_FILL_ALGORITHM
